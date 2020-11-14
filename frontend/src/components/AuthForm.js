1749e82db949e89af796abd9d26a76e7b414fb15
David Shalev <davidshalev1234@gmail.com>
2020-11-15 01:26:31 +0200

    before api call

---
+++ frontend/src/components/AuthForm.js
@@ -0,0 +1,69 @@
+import React,{Component} from "react";
+
+class AuthForm extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            email:"",
+            username:"",
+            password:"",
+            profileImgUrl:""
+        }
+    }
+
+handleChange =(e) => {
+    this.setState({[e.target.name]: e.target.value});
+}
+
+    render(){
+        const {email,username,password,profileImgUrl} = this.state;
+        const {buttonText,heading,signup} = this.props;
+        return(
+            <div>
+                <div className="row justify-content-md-center text-center">
+                    <div className="col-md-6">
+                <form onSubmit={this.handleSubmit}>
+                      <h2>{heading}</h2>
+                      <label htmlFor="email">Email:</label>
+                      <input className="form-control" 
+                        id="email" 
+                        type="text"
+                        onChange={this.handleChange}
+                        value={email}
+                        name="email"
+                        />
+                        <label htmlFor="password">password:</label>
+                      <input className="form-control" 
+                        id="password" 
+                        type="password"
+                        onChange={this.handleChange}
+                        name="password"
+                        />
+                        {signup && (
+                            <div>
+                             <label htmlFor="username">Username:</label>
+                            <input className="form-control" 
+                                id="username" 
+                                type="text"
+                                onChange={this.handleChange}
+                                value={username}
+                                name="username"
+                                />
+                                <label htmlFor="profileImgUrl">profileImgUrl:</label>
+                            <input className="form-control" 
+                                id="profileImgUrl" 
+                                type="text"
+                                onChange={this.handleChange}
+                                name="profileImgUrl"
+                                />
+                            </div>
+                        )}
+                </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default AuthForm;
--- frontend/src/containers/Main.js
+++ frontend/src/containers/Main.js
@@ -1,12 +1,26 @@
-import react from "react";
+import React from "react";
 import {connect} from "react-redux";
 import {withRouter,Redirect,Switch,Route} from "react-router-dom";
 import HomePage from "../components/HomePage";
+import AuthForm from "../components/AuthForm";
+
 const Main = props =>{
     return(
         <div className="container">
             <Switch>
                 <Route exact path="/" render={(props)=><HomePage {...props} /> } />
+                <Route exact path="/signin" render={props => {
+                    return (
+                        <AuthForm 
+                           {...props} heading="Welcome back!!!" buttonText="Sign in"/>
+                    )
+                }} />
+                <Route exact path="/signup" render={props => {
+                    return (
+                        <AuthForm 
+                            signup {...props} heading="welcome!!!" buttonText="Sign up"/>
+                    )
+                }} />
             </Switch>
         </div>
     )
